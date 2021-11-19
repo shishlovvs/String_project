ed3590ecb50bf618a4609c00881cf26c3dd28972
shishlovvs <shishlovvs@mail.ru>
2021-11-19 21:02:34 +0400

    METHODS_OUT_OF_CLASS

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,12 +1,18 @@
-//String
+//string_separation
+
+//constructors_delegation_in_string
 #pragma warning(disable:4326)
 #include<iostream>
-using namespace std;
+//using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;;
 
 #define delimiter "\n--------------------------------------------\n"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////ОБЪЯВЛЕНИЕ КЛАССА - CLASS DECLARATION/////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class String;
 String operator+(const String& left, const String& right);
@@ -16,106 +22,129 @@ class String
 	int size;	//Размер строки в Байтах
 	char* str;	//Адрес строки в динамической памяти
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
 	//			Constructors:
-	String(int size = 80)
-	{
-		this->size = size;
-		this->str = new char[size] {};
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	String(const char* str)
-	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char* str);
 	//			The rule of three
-	String(const String& other)
-	{
-		//Deep copy
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyConstructor:\t" << this << endl;
-	}
-	String(String&& other)
-	{
-		//Shallow copy - Поверхностое копирование
-		//Конструктор переноса должен работать так,
-		//как НЕ должен работать конструктор копиования.
-		this->size = other.size;
-		this->str = other.str;
-		cout << "MoveConstructor:\t" << this << endl;
-		other.str = nullptr;
-		other.size = 0;
-	}
-	~String()
-	{
-		delete[] this->str;
-		cout << "Destructor:\t\t" << this << endl;
-		//Memory leak
-	}
+	String(const String& other);
+	String(String&& other);	
+	~String();
 
 	//				Operators
-	String& operator=(const String& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		//Deep copy:
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssigment:\t\t" << this << endl;
-		//Assignment - Присваивание
-		//Assign - Присвоить
-		return *this;
-	}
-	String& operator=(String&& other)
-	{
-		this->size = other.size;
-		this->str = other.str;
-		other.str = nullptr;
-		other.size;
-		return *this;
-	}
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	String& operator+=(const String& other);
 
-	const char& operator[](int i)const
-	{
-		return str[i];
-	}
-	char& operator[](int i)
-	{
-		return str[i];
-	}
+	const char& operator[](int i)const;
+	char& operator[](int i);
 
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
 
-String operator+(const String& left, const String& right)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////КОНЕЦ ОБЪЯВЛЕНИЕ КЛАССА - CLASS DECLARATION END////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//-------------------------------------------------------------------------------------------------------//
+ 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////ОПРЕДЕЛЕНИЕНИЕ КЛАССА - CLASS DEFINITION///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+int String::get_size()const
 {
-	String buffer = left.get_size() + right.get_size() - 1;
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+//			Constructors:
+ String::String(int size) :size(size), str(new char[size] {})
+{
+	cout << "DefaultConstructor:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)//Делегируем выделение памяти конструктору по умолчанию
+{
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t\t" << this << endl;
+}
+//			The rule of three
+String::String(const String& other) :String(other.str)
+{
+	cout << "CopyConstructor:\t" << this << endl;
+}
+String::String(String&& other) :size(other.size), str(other.str)
+{
+	//Shallow copy - Поверхностое копирование
+	//Конструктор переноса должен работать так,
+	//как НЕ должен работать конструктор копиования.
+	other.str = nullptr;
+	other.size = 0;
+	cout << "MoveConstructor:\t" << this << endl;
+}
+String::~String()
+{
+	delete[] this->str;
+	cout << "Destructor:\t\t" << this << endl;
+	//Memory leak
+}
+
+//				Operators
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	//Deep copy:
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssigment:\t\t" << this << endl;
+	//Assignment - Присваивание
+	//Assign - Присвоить
+	return *this;
+}
+String& String::operator=(String&& other)
+{
+	this->size = other.size;
+	this->str = other.str;
+	other.str = nullptr;
+	other.size;
+	return *this;
+}
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
+
+//::Global scope - глобальное пространство имён
+::String operator+(const String& left, const String& right)
+{
+	String buffer(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
 		//buffer.get_str()[i] = left.get_str()[i];
 		buffer[i] = left[i];
@@ -127,12 +156,17 @@ String operator+(const String& left, const String& right)
 	return buffer;
 }
 
-ostream& operator<<(ostream& os, const String& obj)
+std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////КОНЕЦ ОПРЕДЕЛЕНИЯ КЛАССА - CLASS DEFINITION END////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //#define CONSTRUCTORS_CHECK
+//#define OPERATOR_PLUS_CHECK
 
 void main()
 {
@@ -158,6 +192,7 @@ void main()
 	str1 = str1;
 	cout << str1 << endl;*/
 
+#ifdef OPERATOR_PLUS_CHECK
 	String str1 = "Hello";
 	String str2 = "World";
 	cout << delimiter << endl;
@@ -172,5 +207,24 @@ void main()
 	//Move-методы (Move-semantic) С++ 11
 	//Move-constructor (Конструктор переноса)
 	//Move-assignment  (Оператор присвания переноса)
-	//reference to r-value: Class&&
+	//reference to r-value: Class&&  
+#endif // OPERATOR_PLUS_CHECK
+
+	String str1(25);		//Default constructor;
+	str1.print();
+	String str2 = "Hello";	//Single-argument constructor
+	//cout << str2 << endl;
+	str2.print();
+	String str3("World");	//Single-argument constructor
+	cout << str3 << endl;
+	String str4();	//Здесь НЕ создается объект, объявляется функция str4, 
+					//которая ничего не принимает и возвращает объект класса String
+	//cout << str4 << endl;
+	String str5{};	//Явный вызов конструктора по умолчанию.
+	cout << str5 << endl;
+	String str6{ "Параметры в конструктор можно передавать в фигурных скобках" };
+	cout << str6 << endl;
+	String str7 = str6;
+	cout << str7 << endl;
+	cout << str2 + " " + str3 << endl;
 }
